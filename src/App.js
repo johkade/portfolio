9ed26c96a20d1745cacc8a2b7ca2909d3554d388
johkade <johkade.dev@gmail.com>
2022-02-23 00:53:47 +0100

    -added: initial dummy site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<p>... WIP ...</p>
+				<a
+					className='App-link'
+					href='https://www.linkedin.com/in/johannes-kade-815a5b1a1/'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Find me on LinkedIn
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
