8cf28e76b5de4bb3a581bc0a529f120fe14341a0
johkade <johkade.dev@gmail.com>
2022-05-04 00:30:54 +0200

    -added: CV-Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 
+const CV =
+	'https://s3.eu-central-1.amazonaws.com/jk.public.files/Johannes_Kade.pdf';
+
 function App() {
 	return (
 		<div className='App'>
@@ -10,7 +13,15 @@ function App() {
 					href='https://www.linkedin.com/in/johannes-kade-815a5b1a1/'
 					target='_blank'
 					rel='noopener noreferrer'>
-					Find me on LinkedIn
+					Find me on LinkedIn for now.
+				</a>
+
+				<a
+					className='App-link'
+					href={CV}
+					target='_blank'
+					rel='noopener noreferrer'>
+					Check out my CV
 				</a>
 			</header>
 		</div>
